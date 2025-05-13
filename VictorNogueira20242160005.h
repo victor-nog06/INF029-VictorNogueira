25fb7742d42ef9d7aee07776fdc57089d33d68cf
victor-nog06 <Malhadovictor@gmail.com>
2025-05-12 21:13:48 -0300

    Update and rename PrimeiroUltimoNomeMATRICULA.h to VictorNogueira20242160005.h

--- VictorNogueira20242160005.h
+++ VictorNogueira20242160005.h
@@ -11,10 +11,10 @@
 //  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
 
 //  ----- Dados do Aluno -----
-//  Nome:
-//  email:
-//  Matrícula:
-//  Semestre:
+//  Nome: Victor Gabriel da Silva Nogueira
+//  email: victor@smartbusiness1.com.br
+//  Matrícula: 20242160005
+//  Semestre: 2025.1
 
 //  Copyright © 2016 Renato Novais. All rights reserved.
 // Última atualização: 07/05/2021
